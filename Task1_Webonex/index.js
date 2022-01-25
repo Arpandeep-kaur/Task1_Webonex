b41048d5ffea5a13024073b4d41be3e035b85e8b
Arpandeep-kaur <82841771+Arpandeep-kaur@users.noreply.github.com>
2022-01-25 14:53:00 +0530

    Add files via upload

---
+++ Task1_Webonex/index.js
@@ -0,0 +1,14 @@
+/*
+Create a js file which contains variable declaration for a form which asks following basic details :
+(What is your Name, Email, age, address, phone number, gender,Are you married)
+*/
+
+let name;
+let email;
+let age;
+let address;
+let phone_number;
+let gender;
+let isMarried;
+
+console.log(name,email,age,address,phone_number,gender,isMarried);
